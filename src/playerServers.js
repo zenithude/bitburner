ad3f419ac2bd5fc19bb6f698880c0d5a43941cd0
zenithude <zenithude@protonmail.com>
2022-03-25 20:38:38 +0100

    fixing playerServer.js

--- src/playerServers.js
+++ src/playerServers.js
@@ -158,19 +158,23 @@ export async function main(ns) {
           didChange = false
           purchasedServers = getPurchasedServers(ns)
 
-          if (targetRam > ns.getServerRam(purchasedServers[0]).shift()) {
+          if (targetRam > ns.getServerMaxRam(purchasedServers[0])) {
             if (ns.getServerMoneyAvailable('home') * settings.totalMoneyAllocation >= targetRam * settings.gbRamCost) {
-              let hostname = `pserv-${targetRam}-${createUUID()}`
-
+              
+              let myOldUUID = purchasedServers[0].substring(12,purchasedServers[0].length)
+              let hostname = `pserv-${targetRam}-${myOldUUID}`
+        
               await ns.killall(purchasedServers[0])
               await ns.sleep(10)
-              const serverDeleted = await ns.deleteServer(purchasedServers[0])
-              if (serverDeleted) {
+              let serverDeleted = await ns.deleteServer(purchasedServers[0])
+        
+               if (serverDeleted) {
                 hostname = await ns.purchaseServer(hostname, targetRam)
-
+        
                 if (hostname) {
                   ns.tprint(`[${localeHHMMSS()}] Upgraded: ${purchasedServers[0]} into server: ${hostname} (${targetRam} GB)`)
-
+        
+                  purchasedServers[0] = hostname
                   updateServer(ns, serverMap, hostname)
                   didChange = true
                 }
